971796c0591cc2a6cfc72f25b5ea2069af9e092e
Jardel Junior <55859324+jardeljj@users.noreply.github.com>
2024-09-29 01:25:55 -0300

    Criação da classe MineField e implementando o campo junto com o field com JSX

--- App.js
+++ App.js
@@ -1,28 +1,41 @@
 import React, { Component } from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, SafeAreaView } from 'react-native';
 import params from './src/Params';
-import Field from './src/components/Field';
+import MineField from './src/components/MineField';
+import { creatMinedBoard } from './src/functions';
 
 export default class App extends Component {
+
+  constructor(props) {
+    super(props)
+    this.state = this.createState()
+  }
+
+  minesAmount = () => {
+    const cols = params.getColumnsAmount()
+    const rows = params.getRowsAmount()
+    return Math.ceil(cols * rows * params.difficultLevel)
+  }
+
+  createState = () => {
+    const cols = params.getColumnsAmount()
+    const rows = params.getRowsAmount()
+    return {
+      board: creatMinedBoard(rows, cols, this.minesAmount()),
+    }
+  }
+
+
   render() {
     return (
-      <View style={styles.sectionContainer}>
+      <SafeAreaView style={styles.sectionContainer}>
         <Text >Iniciando o Mines!!</Text>
         <Text >Tamanho da Grade:
-          {params.getRowsAmount()}X{params.getColumnsAmount()}
-        </Text>
-        <Field />
-        <Field opened></Field>
-        <Field opened nearMines={1} />
-        <Field opened nearMines={2} />
-        <Field opened nearMines={3} />
-        <Field opened nearMines={6} />
-        <Field mined />
-        <Field mined opened />
-        <Field mined opened exploded />
-        <Field flagged/>
-        <Field flagged opened/>
-      </View>
+          {params.getRowsAmount()}X{params.getColumnsAmount()}</Text>
+        <View style={styles.board}>
+          <MineField board={this.state.board} />
+        </View>
+      </SafeAreaView>
     )
   }
 }
@@ -30,10 +43,13 @@ export default class App extends Component {
 
 
 const styles = StyleSheet.create({
-  sectionContainer: {
+  container: {
     flex: 1,
-    justifyContent: 'center',
+    justifyContent: 'flex-end',
+  },
+  board: {
     alignItems: 'center',
+    backgroundColor: '#AAA'
   }
 });
 
